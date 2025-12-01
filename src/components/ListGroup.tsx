fd1da9a315b290a801a42a62ebd50403889e9226
salman <salmankhilji010@gmail.com>
2025-12-01 12:38:32 +0500

    Handled events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MouseEvent } from "react";
 const ListGroup = () => {
   let items = [
     "An item",
@@ -7,15 +8,20 @@ const ListGroup = () => {
     "A fourth item",
     "And a fifth one",
   ];
-  items = [];
 
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>My List Group</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No items found.</p>}
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
