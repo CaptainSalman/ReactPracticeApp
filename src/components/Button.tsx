32b0c83279c37f85f596d3f191fed70ca346884d
salman <salmankhilji010@gmail.com>
2025-12-02 10:43:44 +0500

    Added building components pdf

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,24 @@
-
 interface Props {
-    color?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
-    buttonLabel: string;
-    onClick: () => void;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
+  buttonLabel: string;
+  onClick: () => void;
 }
 const Button = ({ color = "primary", buttonLabel, onClick }: Props) => {
   return (
     <div>
-        <button className={`btn btn-${color}`} onClick={onClick}>{buttonLabel}</button>
+      <button className={`btn btn-${color}`} onClick={onClick}>
+        {buttonLabel}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
