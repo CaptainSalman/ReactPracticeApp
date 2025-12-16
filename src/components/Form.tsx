9c62fea71a6b0ae6a55c3a43d1f7939be7a7fc5c
salman <salmankhilji010@gmail.com>
2025-12-16 16:53:34 +0500

    Implemented controlled cnmponents

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,42 @@
-import { use, useRef, type FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 
 const Form = () => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = {
-        name: '', age : 0
-    }
-    const handleSumbit = (event: FormEvent) => {
-        event.preventDefault();
-        person.name = nameRef.current?.value ?? '';     
-        person.age = Number(ageRef.current?.value ?? '0');  
-        console.log(person); 
-    }
+  const [person, setPerson] = useState({
+    name: "",
+    age: '',
+  });
+  const handleSumbit = (event: FormEvent) => {
+    console.log(person);
+    event.preventDefault();
+  };
   return (
-    <form onSubmit={handleSumbit}> 
+    <form onSubmit={handleSumbit}>
       {/* div.mb-3 > label.form-label + input.form-control[type="text"] */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) => {
+            setPerson({ ...person, name: event.target.value });
+          }}
+          id="name"
+          value={person.name}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input onChange={(event)=> {
+          setPerson({...person, age: Number(event.target.value)})
+        }} id="age" value={person.age} type="number" className="form-control" />
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
